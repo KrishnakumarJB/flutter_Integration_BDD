c6ea30e49318049804190e53007eb0a851bce9aa
Ekhart86 <valderama1@yandex.ru>
2022-05-29 09:51:32 +0400

    Initial commit

---
+++ integration_test/steps/login_test_steps.dart
@@ -0,0 +1,6 @@
+import 'package:gherkin/gherkin.dart';
+import '../utils/scenario_context.dart';
+
+class LoginTestSteps {
+  static Iterable<StepDefinitionGeneric<ScenarioContext>> get steps => [];
+}
